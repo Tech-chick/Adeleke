72acb4e2e1295ae5b6206d83a3a4e0c03aabbedb
AdelekeRuth <Adelekeadefunke2442@gmail.com>
2022-09-09 18:04:02 +0100

    Added some images to the project section

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ document.querySelectorAll(".nav-item").forEach(n => n.
       navBar.classList.remove("active");
   }))
 
+  const scriptURL = '  https://script.google.com/macros/s/AKfycbzazhc4QcsuRtaVb6v1FNSoWXkAFmR3Syo9hFc7whdkHwby2ur2MAClYu2CHCLS6gpi/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
 
-
-  
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>  {
+          msg.innerHTML = 'message sent successfully!'
+          setTimeout(function(){
+              msg.innerHTML = ''
+          },5000)
+      } )
+      form.reset()
+      .catch(error => console.error('Error!', error.message))
+  })
+ 
